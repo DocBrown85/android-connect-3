cbd3d619cd969ba893da500c83b7a81a4eb619e9
docbrown85 <me@example.com>
2020-10-26 09:48:41 +0100

    Add drop in counter animation

--- app/src/main/java/com/example/connect3/MainActivity.kt
+++ app/src/main/java/com/example/connect3/MainActivity.kt
@@ -2,8 +2,22 @@ package com.example.connect3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import com.example.connect3.R.drawable.yellow
 
 class MainActivity : AppCompatActivity() {
+
+    fun dropIn(view: View) {
+
+        var counter = view as ImageView
+
+        counter.translationY = -1500f
+        counter.setImageResource(yellow)
+        counter.animate().translationYBy(1500f).rotation(3600f).duration = 1000
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
